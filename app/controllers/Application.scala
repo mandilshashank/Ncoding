ed9243978c5904679f5522e9c1b475709d3a509f
Shashank Mandil <mandil.shashank@gmail.com>
2015-09-13 23:54:22 -0700

    Added the statewise select for the tabular and the graph display

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,5 @@
 package controllers
 
-import play.api.data.Form
 import play.api.db.DB
 import play.api.mvc._
 import anorm._
@@ -8,6 +7,7 @@ import play.api.Play.current
 
 object Application extends Controller {
   var loggedIn = false;
+
   def index = Action {
     Ok(views.html.App.index())
   }
@@ -46,38 +46,57 @@ object Application extends Controller {
   }
 
   def dataPage = Action{ request => {
+      var state = "all";
+
       if(request.body.asFormUrlEncoded != None) {
-        val postParams = request.body.asFormUrlEncoded.get;
+        val postParams = request.body.asFormUrlEncoded.get
         val logout = postParams.get("logout").map(_.head).getOrElse("undefined");
         if (logout == "Logout") {
-          loggedIn = false;
+          loggedIn = false
           Redirect(routes.Application.login("false"))
         }
+
+        state = postParams.get("state").map(_.head).getOrElse("undefined");
       }
 
       if(loggedIn){
-        val start_next:Int = request.getQueryString("start_next").getOrElse("0").toInt;
-        val end_next:Int = request.getQueryString("end_next").getOrElse("25").toInt;
-        val next: String = request.getQueryString("next").getOrElse("");
+        var start_next:Int = request.getQueryString("start_next").getOrElse("0").toInt;
+        var end_next:Int = request.getQueryString("end_next").getOrElse("25").toInt;
+        var next: String = request.getQueryString("next").getOrElse("");
+        if(state=="all") {
+          state = request.getQueryString("state").getOrElse("all")
+        }
 
-        val start_prev:Int = request.getQueryString("start_prev").getOrElse("0").toInt;
-        val end_prev:Int = request.getQueryString("end_prev").getOrElse("25").toInt;
-        val prev: String = request.getQueryString("prev").getOrElse("");
+        var start_prev:Int = request.getQueryString("start_prev").getOrElse("0").toInt;
+        var end_prev:Int = request.getQueryString("end_prev").getOrElse("25").toInt;
+        var prev: String = request.getQueryString("prev").getOrElse("");
 
         DB.withConnection { implicit c =>
-          val findDonation: List[(String, String)] = SQL("Select * from data")().map(row =>
-            row[String]("schoolname") -> row[String]("awardamount")).toList;
+          val findDonation = if(state != "None" && state != "undefined" && state!="all") {
+            SQL("Select * from data where state = '" + state + "'")().map(row => (row[String]("schoolname"), row[String]("awardamount"), row[String]("state"))).toList;
+          } else {
+            SQL("Select * from data")().map(row => (row[String]("schoolname"), row[String]("awardamount"), row[String]("state"))).toList;
+          }
 
-          val findCount: Row = SQL("Select count(*) as count from data").apply().head
+          val findStates = SQL("Select * from data group by state")().map(row => row[String]("state")).toList;
+          val states: List[String] = findStates.distinct
+
+          val findCount: Row =
+            if(state != "None" && state != "undefined" && state!="all") {
+              SQL("Select count(*) as count from data where state = '" + state + "'").apply().head
+            } else {
+              SQL("Select count(*) as count from data").apply().head
+            }
+
           val count: Long = findCount[Long]("count")
 
           //Produce a table here
           if(next.equals("Next")) {
-            Ok(views.html.App.dataRender(findDonation, start_next, end_next, count))
+            Ok(views.html.App.dataRender(findDonation,states, state, start_next, end_next, count))
           } else if(prev.equals("Prev")) {
-            Ok(views.html.App.dataRender(findDonation, start_prev, end_prev, count))
+            Ok(views.html.App.dataRender(findDonation,states, state, start_prev, end_prev, count))
           } else {
-            Ok(views.html.App.dataRender(findDonation, 0, 25, count))
+            Ok(views.html.App.dataRender(findDonation,states, state, 0, 25, count))
           }
         }
       } else {
