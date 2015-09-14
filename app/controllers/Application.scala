745f0d07a225bf3a97d1af74642d210cb16b43d6
Shashank Mandil <mandil.shashank@gmail.com>
2015-09-13 22:29:36 -0700

    Restricted direct access to the datapage. Also added logout button

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,7 +7,7 @@ import anorm._
 import play.api.Play.current
 
 object Application extends Controller {
-
+  var loggedIn = false;
   def index = Action {
     Ok(views.html.App.index())
   }
@@ -16,8 +16,8 @@ object Application extends Controller {
     Ok(views.html.App.sayHello(request.getQueryString("myName").getOrElse("")))
   }
 
-  def login = Action{
-    Ok(views.html.App.login())
+  def login(error: String) = Action{
+    Ok(views.html.App.login(error))
   }
 
   def loginCheck() = Action {
@@ -34,40 +34,54 @@ object Application extends Controller {
         System.out.print(Row);
 
         if(count >= 1){
+          loggedIn=true;
           //User found in database
           Redirect("/dataPage");
         } else {
           //User not found in database
-          Redirect(routes.Application.login())
+          Redirect(routes.Application.login("true"))
         }
       }
     }
   }
 
   def dataPage = Action{ request => {
-    val start_next:Int = request.getQueryString("start_next").getOrElse("0").toInt;
-    val end_next:Int = request.getQueryString("end_next").getOrElse("25").toInt;
-    val next: String = request.getQueryString("next").getOrElse("");
-
-    val start_prev:Int = request.getQueryString("start_prev").getOrElse("0").toInt;
-    val end_prev:Int = request.getQueryString("end_prev").getOrElse("25").toInt;
-    val prev: String = request.getQueryString("prev").getOrElse("");
-
-    DB.withConnection { implicit c =>
-        val findDonation: List[(String, String)] = SQL("Select * from data")().map(row =>
-          row[String]("schoolname") -> row[String]("awardamount")).toList;
-
-        val findCount: Row = SQL("Select count(*) as count from data").apply().head
-        val count: Long = findCount[Long]("count")
-
-        //Produce a table here
-        if(next.equals("Next")) {
-          Ok(views.html.App.dataRender(findDonation, start_next, end_next, count))
-        } else if(prev.equals("Prev")) {
-          Ok(views.html.App.dataRender(findDonation, start_prev, end_prev, count))
-        } else {
-          Ok(views.html.App.dataRender(findDonation, 0, 25, count))
+      if(request.body.asFormUrlEncoded != None) {
+        val postParams = request.body.asFormUrlEncoded.get;
+        val logout = postParams.get("logout").map(_.head).getOrElse("undefined");
+        if (logout == "Logout") {
+          loggedIn = false;
+          Redirect(routes.Application.login("false"))
         }
+      }
+
+      if(loggedIn){
+        val start_next:Int = request.getQueryString("start_next").getOrElse("0").toInt;
+        val end_next:Int = request.getQueryString("end_next").getOrElse("25").toInt;
+        val next: String = request.getQueryString("next").getOrElse("");
+
+        val start_prev:Int = request.getQueryString("start_prev").getOrElse("0").toInt;
+        val end_prev:Int = request.getQueryString("end_prev").getOrElse("25").toInt;
+        val prev: String = request.getQueryString("prev").getOrElse("");
+
+        DB.withConnection { implicit c =>
+          val findDonation: List[(String, String)] = SQL("Select * from data")().map(row =>
+            row[String]("schoolname") -> row[String]("awardamount")).toList;
+
+          val findCount: Row = SQL("Select count(*) as count from data").apply().head
+          val count: Long = findCount[Long]("count")
+
+          //Produce a table here
+          if(next.equals("Next")) {
+            Ok(views.html.App.dataRender(findDonation, start_next, end_next, count))
+          } else if(prev.equals("Prev")) {
+            Ok(views.html.App.dataRender(findDonation, start_prev, end_prev, count))
+          } else {
+            Ok(views.html.App.dataRender(findDonation, 0, 25, count))
+          }
+        }
+      } else {
+        Redirect(routes.Application.login("false"))
       }
     }
   }
